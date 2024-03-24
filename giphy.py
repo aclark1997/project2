40d4621fed32cf621d832f2fb20b119e49b578d9
aclark1997 <123202868+aclark1997@users.noreply.github.com>
2024-03-24 09:52:42 -0600

    added trending tests

--- giphy.py
+++ giphy.py
@@ -10,12 +10,12 @@ class GiphyAPI:
 		self.key = api_key
 	def get_trending(self, count):	
 		if count is None or count > 25 or count < 1:
-			count = 5
+			count = 1
 		result = requests.get("https://api.giphy.com/v1/gifs/trending?api_key="+self.key+"&limit=" + str(count) +"&offset=0&rating=g")
 		return result.json()
 	def get_search(self, count, q):
 		if count is None or count > 25 or count < 1:
-			count = 5
+			count = 1
 		result = requests.get("https://api.giphy.com/v1/gifs/search?api_key="+self.key+"&q=" + q + "&limit=" + str(count) +"&offset=0&rating=g")
 		return result.json()
 
@@ -36,8 +36,8 @@ cli = GiphyCLI()
 @click.group() 
 def gif():
     print("hello from giphy cli!")
-    SoftTests()
-    MediumTests()
+    APITests()
+    CLITests()
 
 
 @gif.command()
@@ -57,16 +57,26 @@ def search(count, markdown, term):
     print("search subcommand called!")
     cli.print_search(count, markdown, term)
 
-def SoftTests(): 
-	print("soft test")
+def CLITests(): 
+	trending = cli.construct_trending(5, False)
+	if len(trending) <= len("https://giphy.com/"): #our message will at least be this long
+		print("CLI TEST FAILED, did not return a constructed list")
+		print("ABORTING CLI TESTS!")
+		return
+	trending = cli.construct_trending(0, False)
+	if trending != "Please request at least 1 result.":
+		print("CLI TEST FAILED, did not return warning when count set to 0")
+	trending = cli.construct_trending(5, True)
+	if len(trending) >= 4 and trending[3] != "!":
+		print("CLI TEST FAILED, did not print in markdown when asked")
 
-def MediumTests():
+def APITests():
 	api = GiphyAPI(API_KEY)
 	resp = api.get_trending(5)
 
 	if resp["meta"]["status"] == 401:
 		print("API TEST FAILED, API KEY INCORRECT")
-		print("ABORTING TESTS")
+		print("ABORTING API TESTS!")
 		exit()
 		return
 
