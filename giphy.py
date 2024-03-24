e9a5f3905ad89c79af1adb66799eba208b7ba539
aclark1997 <123202868+aclark1997@users.noreply.github.com>
2024-03-24 08:05:01 -0600

    implement GiphyAPI and its tests

--- giphy.py
+++ giphy.py
@@ -1,13 +1,59 @@
 import requests
 import os
+import click
 
 API_KEY = os.environ["GIPHY_API_KEY"]
 
-def main():
-    print("hello world")
-    result = requests.get("https://api.giphy.com/v1/gifs/trending?api_key="+API_KEY+"&limit=25&offset=0&rating=g")
-    print(result.text)
 
+class GiphyAPI:
+	def __init__(self, api_key):
+		self.key = api_key
+	def get_trending(self, count):	
+		if count is None or count > 25 or count < 1:
+			count = 5
+		result = requests.get("https://api.giphy.com/v1/gifs/trending?api_key="+self.key+"&limit=" + str(count) +"&offset=0&rating=g")
+		return result.json()
+	def get_search(self, count, q):
+		if count is None or count > 25 or count < 1:
+			count = 5
+		result = requests.get("https://api.giphy.com/v1/gifs/search?api_key="+self.key+"&q=" + q + "&limit=" + str(count) +"&offset=0&rating=g")
+		return result.json()
+
+@click.group()
+def gif():
+    print("hello from giphy cli!")
+    MediumTests()
+
+
+@gif.command()
+def trending():
+    print("trending subcommand called!")
+    api = GiphyAPI(API_KEY)
+
+
+@gif.command()
+def search():
+    print("search subcommand called!")
+    api = GiphyAPI(API_KEY)
+
+
+def MediumTests():
+	api = GiphyAPI(API_KEY)
+	resp = api.get_trending(5)
+
+	if resp["meta"]["status"] == 401:
+		print("TEST FAILED, API KEY INCORRECT")
+		print("ABORTING TESTS")
+		exit()
+		return
+
+	if resp["pagination"]["total_count"] < 5:
+		print("TEST FAILED, TRENDING DID NOT RETURN AT LEAST 5 RESULTS")
+
+	resp = api.get_search(5, "cat")
+
+	if resp["meta"]["status"] != 200:
+		print("TEST FAILED, STATUS RETURNED FROM SEARCH NOT 200/OK")
 
 if __name__ == "__main__":
-    main()
+    gif()
